bc3c6085ceebc078bd7a4a4a02f142ee100844b7
alfan <zahgreat30@gmail.com>
2024-12-19 21:45:56 +0700

    update configuration

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import express, {
-  type NextFunction,
-  type Request,
-  type Response,
-  type Application
-} from 'express'
+import express, { NextFunction, Request, Response, Application } from 'express'
 
 import 'dotenv/config'
 
 const app: Application = express()
-const port: number =
-  process.env.PORT != null ? parseInt(process.env.PORT) : 3000
+const port: number = process.env.PORT != null ? parseInt(process.env.PORT) : 3000
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello, World! UIO')
+})
+app.get('/health', (req: Request, res: Response, next: NextFunction) => {
+  const result = { data: 'Health' }
+  res.send(result).status(200)
 })
 
 app.listen(port, () => {
