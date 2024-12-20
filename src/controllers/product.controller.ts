e5ebeefaa22f34466ea2569641bd8d79f2ab8ee3
alfan <zahgreat30@gmail.com>
2024-12-20 10:05:22 +0700

    add Product controller

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express'
+import { createProductValidationSchema } from '../validations/product.validation'
+import { logger } from '../utils/logger'
+
+interface Product {
+  id: number
+  name: string
+  desc: string
+}
+
+export const getProducts = (req: Request, res: Response, next: NextFunction) => {
+  const products: Product[] = []
+  products.push({ id: 1, name: 'Xbox', desc: 'Description 1' } as Product)
+  products.push({ id: 2, name: 'PS4', desc: 'Description 2' } as Product)
+  products.push({ id: 3, name: 'Laptop', desc: 'Description 1' } as Product)
+  products.push({ id: 4, name: 'Iphone 15', desc: 'Description 2' } as Product)
+
+  const {
+    params: { name }
+  } = req
+
+  if (name) {
+    const filterProducts = products.filter((product) => {
+      if (product.name === name) return product
+    })
+    logger.info(`Filtered products by name: ${name}`)
+    if (filterProducts.length === 0) {
+      console.log('🚀 ~ getProducts ~ filterProducts.length:', filterProducts.length)
+      res.status(404).send({ message: 'Product not found', data: [] })
+    }
+    res.status(200).send({ message: '', data: filterProducts[0] })
+  }
+
+  res.status(200).send({ message: '', data: products })
+}
+
+export const createProduct = (req: Request, res: Response, next: NextFunction) => {
+  const { error, value } = createProductValidationSchema(req.body)
+  if (error) {
+    logger.error(`Product Validation error: ${error.details[0].message}`)
+    res.status(422).send({ message: error.details[0].message, data: null })
+  }
+
+  res.status(201).send({ message: 'Product created successfully', data: value })
+}
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,29 +1,8 @@
-import { NextFunction, Request, Response, Router } from 'express'
-import { createProductValidationSchema } from '../validation/product.validation'
-import { logger } from '../utils/logger'
+import { Router } from 'express'
+import { createProduct, getProducts } from '../controllers/product.controller'
 
 export const ProductRouter = Router()
 
-interface Product {
-  id: number
-  name: string
-  desc: string
-}
-
-ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  const products: Product[] = []
-  products.push({ id: 1, name: 'Product 1', desc: 'Description 1' } as Product)
-  products.push({ id: 2, name: 'Product 2', desc: 'Description 2' } as Product)
-
-  res.status(200).send({ message: '', data: products })
-})
-
-ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
-  const { error, value } = createProductValidationSchema(req.body)
-  if (error) {
-    logger.error(`Product Validation error: ${error.details[0].message}`)
-    res.status(422).send({ message: error.details[0].message, data: null })
-  }
-
-  res.status(201).send({ message: 'Product created successfully', data: value })
-})
+ProductRouter.get('/', getProducts)
+ProductRouter.get('/:name', getProducts)
+ProductRouter.post('/', createProduct)
